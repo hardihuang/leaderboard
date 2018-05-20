9368da48deac326f9573cd0467b100169dcfbe9c
haridhuang@outlook.com <hardihuang@outlook.com>
2018-05-20 18:24:07 +0800

    data in an array

---
+++ local test/loop_version/loop_version.ino
@@ -0,0 +1,168 @@
+#include "LedControl.h"
+LedControl lc=LedControl(12,11,10,3);
+char scores[] = {5, 10, 15, 20, 25, 30, 35, 40};
+
+void setup() {
+  for(int index=0; index<lc.getDeviceCount();index++){
+    lc.shutdown(index, false);
+    lc.setIntensity(index, 15);
+  }
+  clearDigits();
+	
+}
+
+void loop() {
+  //clearDigits();
+	
+	for(int index=0; index<8; index++){
+    Display(index+1, index+1, scores[index]);
+  }
+  
+
+  /*
+  for(int i=0;i<8;i++){
+    clearDigits();
+    Display(i+1, 1, 111);
+    delay(10);
+  }*/
+
+  /*
+  for(int i=0;i<8;i++){
+    scores[i] = scores[i]+1;
+  }
+  */
+  delay(500);
+}
+
+
+int Display(int row, int group, int score){
+  int addr1;
+  int addr2;
+  int addr3;
+  int digit1;
+  int digit2;
+  int digit3;
+  int value1;
+  int value2;
+  int value3;
+  boolean dp2;
+  
+  
+	switch(row){
+
+		case 1:	
+			addr1 = 0;	
+			digit1 = 0;	
+			addr2 = 0;
+			digit2 = 1;
+			addr3 = 0;
+			digit3 = 2;
+			break;
+			
+		case 2:
+			addr1 = 0;
+			digit1 = 3;
+			addr2 = 0;
+			digit2 = 4;
+			addr3 = 0;
+			digit3 = 5;
+			break;
+     
+		case 3:
+			addr1 = 1;
+			digit1 = 0;
+			addr2 = 0;
+			digit2 = 6;
+			addr3 = 0;
+			digit3 = 7;
+			break;
+      
+		case 4:
+			addr1 = 1;
+			digit1 = 1;
+			addr2 = 1;
+			digit2 = 2;
+			addr3 = 1;
+			digit3 = 3;
+			break;
+      
+		case 5:
+			addr1 = 1;
+			digit1 = 4;
+			addr2 = 1;
+			digit2 = 5;
+			addr3 = 1;
+			digit3 = 6;
+			break;
+      
+		case 6:
+			addr1 = 1;
+			digit1 = 7;
+			addr2 = 2;
+			digit2 = 0;
+			addr3 = 2;
+			digit3 = 1;
+			break;
+      
+		case 7:
+			addr1 = 2;
+			digit1 = 2;
+			addr2 = 2;
+			digit2 = 3;
+			addr3 = 2;
+			digit3 = 4;
+			break;
+      
+		case 8:
+			addr1 = 2;
+			digit1 = 5;
+			addr2 = 2;
+			digit2 = 6;
+			addr3 = 2;
+			digit3 = 7;
+			break;
+		
+	}
+	
+	value1 = group;	
+	value3 = GetNumber(score,0);
+	value2 = GetNumber(score,1);
+	dp2 = GetNumber(score,2);
+	
+	lc.setDigit(addr1, digit1, (byte)value1, 0);	//group digit
+	lc.setDigit(addr2, digit2, (byte)value2, dp2);	//score tens digit
+	lc.setDigit(addr3, digit3, (byte)value3, 0);	//score ones digit
+  //lc.setDigit(0, 0, (byte)1, 0);
+}
+
+//places: 0=ones 1=tens 2=hundreds
+int GetNumber(int v, int places){
+	int ones;  
+  int tens;  
+  bool hundreds; 
+
+  if(v <= 0 || v > 198){
+		return;  
+	}
+    ones=v%10;  
+    v=v/10;  
+    tens=v%10;  
+    v=v/10; 
+    hundreds=v;  
+	
+	if(places == 0){
+	    return ones;
+	}else if(places == 1){
+	    return tens;
+	}else if(places == 2){
+	    return hundreds;
+	}else{
+	    return;  
+	}
+}
+
+void clearDigits(){
+  for(int index=0; index<lc.getDeviceCount();index++){
+    lc.clearDisplay(index);
+  }
+}
